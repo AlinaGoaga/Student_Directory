e96147768c3ecf065df00d302e2e07fc9bfdea30
Alina Goaga <goaga.alina@gmail.com>
2018-11-13 14:48:09 +0000

    Exercise2 name starts with a specific letter

--- directory.rb
+++ directory.rb
@@ -24,7 +24,9 @@ end
 
 def print(students)
   students.each_with_index do |student,i|
+    if student[:name].start_with? 'A'
     puts "#{i+1}.  #{student[:name]} (#{student[:cohort]} cohort)"
+  end
   end
 end
 
