967710de59e47c93d0ae75a6fa14368866a059d9
Alina Goaga <goaga.alina@gmail.com>
2018-11-18 17:10:38 +0000

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,13 +1,10 @@
 def input_students
   puts "Please enter the names of the students, their country of birth and main hobby"
   puts "To finish, just hit return twice"
-  # create an empty array
   students = []
-  # get the first name
   puts "Please insert the student's name"
   name = gets.strip
 
-  # while the name is not empty, repeat this code
   while !name.empty? do
     puts "Please insert the student's country of birth"
     country_of_birth = gets.strip
@@ -29,6 +26,7 @@ def input_students
   end
   # return the array of students
   students
+
 end
 
 def print_header
@@ -62,9 +60,32 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(100) if names.count != 0
 end
-#nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
-print_grouped_by_cohort(students)
+
+# print_grouped_by_cohort(students)
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students # this will be the array of hashes
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
